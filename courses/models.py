5e4bd1e7b88486d48a0f728c5701e9da75e2f528
Bulgaru Viorel <bulgaru.viorel.tudor@gmail.com>
2023-12-12 20:52:41 +0200

    adde a test comment

--- courses/models.py
+++ courses/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from pricing.models import Subscription
-
+# added comment
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
